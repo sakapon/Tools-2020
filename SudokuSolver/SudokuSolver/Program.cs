f519b096715a5157027150fa8d920ae48a70e1c6
Keiho Sakapon <git@saka-pon.net>
2020-04-07 23:31:04 +0900

    Detect UTF-8

--- SudokuSolver/SudokuSolver/Program.cs
+++ SudokuSolver/SudokuSolver/Program.cs
@@ -11,21 +11,20 @@ namespace SudokuSolver
 		const string Input = "Input";
 		const string Output = "Output";
 
-		static readonly Encoding UTF8N = new UTF8Encoding();
-
 		static void Main(string[] args)
 		{
 			if (!Directory.Exists(Input)) return;
 			Directory.CreateDirectory(Output);
 
-			foreach (var file in Directory.EnumerateFiles(Input))
+			foreach (var filePath in Directory.EnumerateFiles(Input))
 			{
 				try
 				{
-					var fileName = Path.GetFileName(file);
+					var fileName = Path.GetFileName(filePath);
 					Console.WriteLine(fileName);
 
-					var input = File.ReadAllLines(file, UTF8N);
+					var encoding = DetectEncoding(filePath);
+					var input = File.ReadAllLines(filePath, encoding);
 
 					var sw = Stopwatch.StartNew();
 					var solution = Solver.Solve(input);
@@ -33,13 +32,27 @@ namespace SudokuSolver
 					Console.WriteLine(sw.Elapsed);
 
 					var content = string.Join("", solution.Select(x => x + "\n"));
-					File.WriteAllText(Path.Combine(Output, fileName), content, UTF8N);
+					File.WriteAllText(Path.Combine(Output, fileName), content, encoding);
 				}
 				catch (Exception ex)
 				{
 					Console.WriteLine(ex.Message);
 				}
 			}
+		}
+
+		static readonly Encoding UTF8N = new UTF8Encoding();
+
+		static Encoding DetectEncoding(string filePath)
+		{
+			var preamble = Encoding.UTF8.GetPreamble();
+			var headBytes = new byte[preamble.Length];
+
+			using (var stream = File.OpenRead(filePath))
+			{
+				stream.Read(headBytes, 0, headBytes.Length);
+			}
+			return headBytes.SequenceEqual(preamble) ? Encoding.UTF8 : UTF8N;
 		}
 	}
 }
